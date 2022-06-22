f277f54e45fd6a21c5a1495365de325ea7b872c5
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-06-22 16:32:41 +0200

    palindrome done

--- js/main.js
+++ js/main.js
@@ -1,31 +1,11 @@
 
+// ! PAROLE PALINDROME ! \\
+    
 // creo una costante che chieda una parola all'utente
 const userWord = prompt('Inserisci una parola Palindroma');
+    
+const reverseWord = wordReverse(userWord);
 
-// creo una variabile per metterci le lettere della parola
-let letterWord;
-
-// creo una variabile per visualizzare la parola al contrario
-let reverseWord = '';
-
-// creo il contatore per il ciclo while
-let lengthWord = userWord.length - 1;
-
-
-// creo il ciclo while che parte dalla fine
-while ( lengthWord >= 0 ) {
-
-    // riassegno la variabile letter alla lunghezza di userWord - 1
-    letterWord = userWord[lengthWord];
-
-    // sommo a reverseword ogni carattere della parola inserita
-   reverseWord += letterWord;
-
-   
-   lengthWord--;
-}
-
-console.log(reverseWord);
 
 // creo un if per verificare che la parola sia palindroma
 if (userWord == reverseWord) {
@@ -37,3 +17,33 @@ if (userWord == reverseWord) {
     console.log('la parola non è palindroma')
 
 }
+
+
+
+// * FUNCTION * \\
+function wordReverse (word) {
+    
+    // creo il contatore per il ciclo while
+    let lengthWord = userWord.length - 1;
+    
+    // creo una variabile per visualizzare la parola al contrario
+    let wordFlip = '';
+    
+    // creo il ciclo while che parte dalla fine
+    while ( lengthWord >= 0 ) {
+        
+        // sommo a reverseword ogni carattere della parola inserita
+        wordFlip += userWord[lengthWord];
+        
+        
+        lengthWord--;
+    }
+    
+    console.log(wordFlip);
+
+    // INPUT RITORNO ALTRIMENTI L'IF NON FUNZIONA
+    return wordFlip;
+}
+// * FUNCTION * \\
+
+// ! PAROLE PALINDROME ! \\
